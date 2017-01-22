742c0850052be897e0401561268a00c0fa4a83db
Surma <surma@surma.link>
2017-01-22 15:04:04 +0000

    Switch to s3put

--- tools/pr_preview.sh
+++ tools/pr_preview.sh
@@ -8,7 +8,6 @@ NAME=pr_${TRAVIS_PULL_REQUEST}
 
 npm i
 npm run build
-mv docs $NAME
-mkdir docs
-mv $NAME docs/
-
+go get github.com/surma/s3put
+cd docs
+s3put -b s3://s3-eu-west-1.amazonaws.com/${ARTIFACTS_BUCKET} -k ${ARTIFACTS_KEY} -s ${ARTIFACTS_SECRET} -p pr/${TRAVIS_PULL_REQUEST} put .
