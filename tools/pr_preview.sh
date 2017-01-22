cfc15bcb88f03dfb0605bd683ec14a4c7f8c70ad
Surma <surma@surma.link>
2017-01-22 16:00:12 +0000

    lol10

--- tools/pr_preview.sh
+++ tools/pr_preview.sh
@@ -8,4 +8,4 @@ npm i
 npm run build
 go get github.com/surma/s3put
 cd docs
-s3put -b s3://s3-eu-west-1.amazonaws.com/${ARTIFACTS_BUCKET} -k ${ARTIFACTS_KEY} -s ${ARTIFACTS_SECRET} -p pr/${TRAVIS_PULL_REQUEST} put .
+s3put -b s3://s3-eu-west-1.amazonaws.com/${ARTIFACTS_BUCKET} -k ${ARTIFACTS_KEY} -s ${ARTIFACTS_SECRET} -p ${NAME} put .
