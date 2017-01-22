918c95017dd1a5bdfb64d1848b017ac5f3853f49
Surma <surma@surma.link>
2017-01-22 15:04:04 +0000

    Switch to s3put

--- tools/pr_preview.sh
+++ tools/pr_preview.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -o
+set -e
 
-# Exit if this is not a pull request
-$TRAVIS_PULL_REQUEST || exit 0
-NAME=pr_${TRAVIS_PULL_REQUEST}
+NAME=${TRAVIS_COMMIT}
 
+. ~/.nvm/nvm.sh
+nvm install 7
+nvm use 7
 npm i
 npm run build
-mv docs $NAME
-mkdir docs
-mv $NAME docs/
-
+wget https://tools.surma.link/s3put/s3put_linux_amd64
+chmod +x s3put_linux_amd64
+cd docs
+../s3put_linux_amd64 -b s3://s3-eu-west-1.amazonaws.com/${ARTIFACTS_BUCKET} -k ${ARTIFACTS_KEY} -s ${ARTIFACTS_SECRET} -p ${NAME} put .
