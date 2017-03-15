0f666d01f9f9ed978a9ce5a829f9be150a00350b
Rob Dodson <robdodson@google.com>
2017-03-15 16:37:19 +1100

    Comments and unit tests

--- elements/dash-checkbox/dash-checkbox.unittest.js
+++ elements/dash-checkbox/dash-checkbox.unittest.js
@@ -1,27 +1,36 @@
-const expect = chai.expect; // FIXME: expect has already been declared
+(function() {
+  const expect = chai.expect;
 
-describe('dash-checkbox', function() {
-  before(dashElements.before());
-  after(dashElements.after());
-  beforeEach(function() {
-    this.container.innerHTML = `
-      <dash-checkbox></dash-checkbox>
-    `;
-    dashElements.waitForElement('dash-checkbox')
-      .then(_ => {
-        this.checkbox = this.container.querySelector('dash-checkbox');
+  describe('dash-checkbox', function() {
+    before(dashElements.before());
+    after(dashElements.after());
+    beforeEach(function() {
+      this.container.innerHTML = `<dash-checkbox></dash-checkbox>`;
+      return dashElements.waitForElement('dash-checkbox')
+        .then(_ => {
+          this.checkbox = this.container.querySelector('dash-checkbox');
+        });
+    });
+
+    it('should add a `role` to the checkbox', function() {
+      expect(this.checkbox.getAttribute('role')).to.equal('checkbox');
+    });
+
+    it('should set `aria-checked` to `false`', function() {
+      expect(this.checkbox.getAttribute('aria-checked')).to.equal('false');
+    });
+
+    it('should add a `tabindex` to the checkbox', function() {
+      expect(this.checkbox.getAttribute('tabindex')).to.equal('0');
+    });
+
+    it('should toggle `checked` and `aria-checked` when calling ' +
+      '`_toggleChecked`', function() {
+        expect(this.checkbox.getAttribute('aria-checked')).to.equal('false');
+        expect(this.checkbox.checked).to.equal(false);
+        this.checkbox._toggleChecked();
+        expect(this.checkbox.getAttribute('aria-checked')).to.equal('true');
+        expect(this.checkbox.checked).to.equal(true);
       });
   });
-
-  it('should add a `role` to the checkbox', function() {
-    expect(this.checkbox.getAttribute('role')).to.equal('checkbox');
-  });
-
-  it('should set `aria-checked` to `false`', function() {
-    expect(this.checkbox.getAttribute('aria-checked')).to.equal('false');
-  });
-
-  it('should add a `tabindex` to the checkbox', function() {
-    expect(this.checkbox.getAttribute('tabindex')).to.equal('0');
-  });
-});
+})();
