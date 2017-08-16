941e83d9a863deefabaeb1052cc4e015811b2748
Rob Dodson <robdodson@google.com>
2017-08-16 12:22:19 -0700

    Update build script to match new web fundamentals structure

--- build-webfundamentals.sh
+++ build-webfundamentals.sh
@@ -2,9 +2,9 @@
 
 npm run build
 rm -rf webfundamentals
-mkdir -p webfundamentals/src/content/en/fundamentals/architecture/howto-components
-find docs -type d -name 'howto-*' | while read line; do cp -r $line/*.md webfundamentals/src/content/en/fundamentals/architecture/howto-components/; done
-cp docs/styles/main.devsite.css webfundamentals/src/content/en/fundamentals/architecture/howto-components/main.css
-cp -r docs/images webfundamentals/src/content/en/fundamentals/architecture/howto-components/
-cat site-resources/static-header.md overview.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/howto-components/overview.md
-cat site-resources/static-header.md glossary.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/howto-components/glossary.md
+mkdir -p webfundamentals/src/content/en/fundamentals/architecture/building-components/examples
+find docs -type d -name 'howto-*' | while read line; do cp -r $line/*.md webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/; done
+cp docs/styles/main.devsite.css webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/main.css
+cp -r docs/images webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/
+cat site-resources/static-header.md overview.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/overview.md
+cat site-resources/static-header.md glossary.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/glossary.md
