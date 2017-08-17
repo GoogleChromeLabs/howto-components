e71893b3a29be40a33be3c30ad6665381fb5f127
Surma <surma@surma.link>
2017-08-17 13:12:02 +0100

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint max-len: ["off"], no-console: ["off"], require-jsdoc: 0 */
 module.exports = function(config) {
   const configuration = {
     basePath: '',
