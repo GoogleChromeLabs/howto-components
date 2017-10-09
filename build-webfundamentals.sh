ab790d04144309752108ceac4f9341d96460ae93
Rob Dodson <robdodson@google.com>
2017-10-09 11:25:05 -0700

    Tidy up some webfudamentals stuff

--- build-webfundamentals.sh
+++ build-webfundamentals.sh
@@ -2,9 +2,7 @@
 
 npm run build
 rm -rf webfundamentals
-mkdir -p webfundamentals/src/content/en/fundamentals/architecture/building-components/examples
-find docs -type d -name 'howto-*' | while read line; do cp -r $line/*.md webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/; done
-cp docs/styles/main.devsite.css webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/main.css
-cp -r docs/images webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/
-cat site-resources/static-header.md overview.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/overview.md
-cat site-resources/static-header.md glossary.md | sed "s/{%DATE%}/$(date +%Y-%m-%d)/" > webfundamentals/src/content/en/fundamentals/architecture/building-components/examples/glossary.md
+mkdir -p webfundamentals/src/content/en/fundamentals/web-components/examples
+find docs -type d -name 'howto-*' | while read line; do cp -r $line/*.md webfundamentals/src/content/en/fundamentals/web-components/examples/; done
+cp docs/styles/main.devsite.css webfundamentals/src/content/en/fundamentals/web-components/examples/main.css
+cp -r docs/images webfundamentals/src/content/en/fundamentals/web-components/examples/
